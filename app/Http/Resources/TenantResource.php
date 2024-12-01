e2eded3386ec125b9e3c01419da6080472eff1e1
Nahuel <nahuefer173@gmail.com>
2024-11-30 22:45:21 -0300

    Add TenantResource

---
+++ app/Http/Resources/TenantResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TenantResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'website' => $this->website,
+            'email' => $this->email,
+            'business_name' => $this->business_name,
+            'verified_email' => $this->verified_email,
+            'account_active' => $this->account_active,
+        ];
+    }
+}
