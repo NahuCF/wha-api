952ab4b2abddcb414e80503b16c93be3a2784cde
Nahuel <nahuefer173@gmail.com>
2024-12-04 22:54:08 -0300

    Pint

--- database/migrations/landlord/2024_12_05_014113_insert_countries.php
+++ database/migrations/landlord/2024_12_05_014113_insert_countries.php
@@ -10,7 +10,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
         $countries = [
             ['name' => 'Afghanistan', 'code' => 'AFG'],
             ['name' => 'Albania', 'code' => 'ALB'],
