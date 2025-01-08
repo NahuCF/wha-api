48d30772b63e967f525592ccd636e4d911dacecb
Nahuel <nahuefer173@gmail.com>
2025-01-08 16:58:31 -0300

    Add tenancy package

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,19 +2,14 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Spatie\Multitenancy\Contracts\IsTenant;
-use Spatie\Multitenancy\Models\Concerns\ImplementsTenant;
-use Spatie\Multitenancy\Models\Concerns\UsesLandlordConnection;
+use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
+use Stancl\Tenancy\Contracts\TenantWithDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDatabase;
+use Stancl\Tenancy\Database\Concerns\HasDomains;
 
-class Tenant extends Model implements IsTenant
+class Tenant extends BaseTenant implements TenantWithDatabase
 {
-    use HasFactory;
-    use HasUuids;
-    use ImplementsTenant;
-    use UsesLandlordConnection;
+    use HasDatabase, HasDomains;
 
     protected $guarded = [];
 }
