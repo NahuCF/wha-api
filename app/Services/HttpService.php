1523ed641fdda59af40fece542a9a77dadafff21
Nahuel <nahuefer173@gmail.com>
2025-07-21 18:17:35 +0000

    feat: refactor Httpservice

--- app/Services/HttpService.php
+++ app/Services/HttpService.php
@@ -8,21 +8,32 @@ class HttpService
 {
     public function __construct(protected string $baseUrl) {}
 
-    public function request(string $method, string $endpoint, array $params = [], array $headers = []): Response
+    public function request(string $method, string $endpoint, array $params = [], array $headers = []) 
     {
         $url = "{$this->baseUrl}/{$endpoint}";
 
         $request = Http::withHeaders($headers)
-            ->acceptJson()
-            ->asForm();
+            ->acceptJson();
 
-        $response = $request->$method($url, $params);
+        if (strtolower($method) === 'get') {
+            $request = $request->withOptions(['query' => $params]);
 
-        return $response->throw();
+            return $request->get($url)->throw();
+        }
+
+        $request = $request->asForm();
+
+        return $request->throw();
     }
 
     public function get(string $endpoint, array $params = []): array
     {
         return $this->request('get', $endpoint, $params)->json();
+
+    }
+
+    public function post(string $endpoint, array $params = []): array
+    {
+        return $this->request('post', $endpoint, $params)->json();
     }
 }
