12912ed6e93889b724ce16640fd0b91d0a694f96
Nahuel <nahuefer173@gmail.com>
2025-08-19 23:56:31 +0000

    fix: unique keys in businesses

--- database/migrations/2024_12_08_012724_create_businesses_table.php
+++ database/migrations/2024_12_08_012724_create_businesses_table.php
@@ -14,9 +14,11 @@ return new class extends Migration
         Schema::create('businesses', function (Blueprint $table) {
             $table->ulid('id')->primary();
             $table->foreignUlid('tenant_id')->constrained()->onDelete('cascade');
-            $table->string('meta_business_id')->unique();
+            $table->string('meta_business_id');
             $table->string('name');
             $table->timestamps();
+
+            $table->unique(['tenant_id', 'meta_business_id']);
         });
     }
 
